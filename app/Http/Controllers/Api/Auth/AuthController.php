31b84c52e4b4dec4193fd0cdd6af07642ee75791
TomyLingga <tomy.troller@gmail.com>
2024-01-30 16:47:49 +0700

    token 1 minggu

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -40,7 +40,7 @@ class AuthController extends Controller
             'foto' => $user->foto,
             'signature' => $user->signature,
             'iat' => time(),
-            'exp' => time() + (20 * 60 * 60)
+            'exp' => time() + (7 * 24 * 60 * 60)
         ];
 
         $token = JWT::encode($payload, env('JWT_SECRET'), 'HS256');
